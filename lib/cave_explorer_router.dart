061ad803ac8bcd8c96d2953dc576b74b9a9a9b8a
SEGVeenstra <seg.veenstra@gmail.com>
2025-03-23 20:56:52 +0100

    Add comments

--- lib/cave_explorer_router.dart
+++ lib/cave_explorer_router.dart
@@ -14,18 +14,25 @@ class CaveExplorerRouter extends GoRouter {
         routingConfig: ValueNotifier(RoutingConfig(routes: _routes)),
       );
 
+  // NOTE: I like to add my own methods to the router for navigation.
+  // They are basically shortcuts for navigating to specif routes.
+  // I prefer this because it keeps the navigation logic in one place, the router.
   void goToEntry() => goNamed('entry');
   void goToTreasure() => goNamed('treasure');
   void goToRoom(String name) => goNamed(name);
 
+  // NOTE: Adding this static function makes it easy to get the router from the context.
   static CaveExplorerRouter of(BuildContext context) =>
       GoRouter.of(context) as CaveExplorerRouter;
 }
 
+// NOTE: With this extension, using the router is even more pretty!
+// Instead of `CaveRouter.of(context)`, you can use `context.caveRouter`.
 extension CaveExplorerRouterExt on BuildContext {
   CaveExplorerRouter get caveRouter => CaveExplorerRouter.of(this);
 }
 
+// NOTE: These are all the routes to the app.
 final _routes = <RouteBase>[
   GoRoute(
     name: 'entry',
